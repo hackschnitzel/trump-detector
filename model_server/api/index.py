80cb99661b286fa885c85c6fea3ea05bebea3201
Jan Steinke <jan.steinke@gmail.com>
2018-09-15 15:26:03 +0200

    only send the fake score

--- model_server/api/index.py
+++ model_server/api/index.py
@@ -42,7 +42,7 @@ def map_foo():
 
 @api.route("/check", methods=["POST"])
 def check():
-    score = str(model.predict_proba(
+    score = model.predict_proba(
         pd.DataFrame(
 
             {
@@ -50,5 +50,5 @@ def check():
                 'text': [request.form['Text']]
             }
         )
-    )[0])
+    )[0][0]
     return jsonify({"score": score})
