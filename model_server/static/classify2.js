c71bea32aeee03cbda3b3ff073d9ef6ffee716e8
Jan Steinke <jan.steinke@gmail.com>
2018-09-15 14:45:39 +0200

    fix javascript getting called instead of submit in HTML

--- model_server/static/classify2.js
+++ model_server/static/classify2.js
@@ -1,31 +1,5 @@
-alert('hlallas')
-
-
-document.onload=function(){
+window.onload=function(){
     document.getElementById('mybutton').addEventListener('click', function (event) {
-        event.preventDefault();
-
-
-        // Do something
-        //updateProperties();
-        var newName = 'John Smith',
-            xhr = new XMLHttpRequest();
-
-        var formData = new FormData(document.getElementById("trumpette"));
-
-
-//xhr.open('POST', 'check?Title=foo&Text=bar');
-        xhr.onload = function () {
-            if (xhr.status === 200 && xhr.responseText !== newName) {
-                alert('Something went wrong.  Name is now ' + xhr.responseText);
-            }
-            else if (xhr.status !== 200) {
-                alert('Request failed.  Returned status of ' + xhr.status);
-            }
-        };
-        xhr.send(formData);
-        return false;
+        alert("foo")
     });
-
-
 }
