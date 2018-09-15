6b13979cc8ba10ab378e6f751d157dac68c19267
Jan Steinke <jan.steinke@gmail.com>
2018-09-15 15:14:48 +0200

    return valid json

--- model_server/api/index.py
+++ model_server/api/index.py
@@ -42,7 +42,7 @@ def map_foo():
 
 @api.route("/check", methods=["POST"])
 def check():
-    return str(model.predict_proba(
+    return {"score": str(model.predict_proba(
         pd.DataFrame(
 
             {
@@ -50,4 +50,4 @@ def check():
                 'text': [request.form['Text']]
             }
         )
-    )[0])
+    )[0])}
