67f016fbcfaca033046b3cc50985d32e934fb64f
Jan Steinke <jan.steinke@gmail.com>
2018-09-15 11:15:23 +0200

    set template dir

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask import Flask
 from model_server.frontend.index import fe
 from model_server.api.index import api
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='model_server/templates/')
 app.register_blueprint(fe, url_prefix='/')
 app.register_blueprint(api, url_prefix='/api')
 
