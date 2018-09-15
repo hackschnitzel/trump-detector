49ef10bb46cfb0c38d729c7daac7d06d4a836347
Frederic Schmaljohann <f.schmaljohann@gmail.com>
2018-09-15 14:54:37 +0200

    cleanup the form

--- model_server/static/classify2.js
+++ model_server/static/classify2.js
@@ -1,5 +1,19 @@
 window.onload=function(){
-    document.getElementById('mybutton').addEventListener('click', function (event) {
-        alert("foo")
+    document.getElementById('submitbutton').addEventListener('click', function (event) {
+        do_ajax()
     });
+
+    function do_ajax() {
+        var req = new XMLHttpRequest();
+        var result = document.getElementById('isfake');
+        req.onreadystatechange = function()
+        {
+            if(this.readyState === 4 && this.status === 200) {
+                result.innerHTML = this.responseText
+            }
+        };
+        req.open('POST', '/api/check', true);
+        req.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+        req.send("Title=" + document.getElementById('Title').value + "&Text=" + document.getElementById('Text').value);
+    }
 }
