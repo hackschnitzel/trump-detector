be38dd1e10e649a78b280dd1689093aab24e739f
Jan Steinke <jan.steinke@gmail.com>
2018-09-15 11:11:40 +0200

    fix the python module order

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
+
 from flask import Flask
 from model_server.frontend.index import fe
 from model_server.api.index import api
-
 
 app = Flask(__name__)
 app.register_blueprint(fe, url_prefix='/')
 app.register_blueprint(api, url_prefix='/api')
 
-
-
+if __name__ == '__main__':
+    app.run()
--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-export FLASK_APP=src/model_server/server.py
-flask run
+./server.py
